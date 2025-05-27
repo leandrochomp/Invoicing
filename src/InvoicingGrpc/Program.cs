c1046b6df7efc4b59eb4fabcdef94ceaab1d6903
leandrochomp <chomp.vercetti@gmail.com>
2025-05-27 16:06:07 +1000

    wip nodaTime to grpc

--- src/InvoicingGrpc/Program.cs
+++ src/InvoicingGrpc/Program.cs
@@ -1,16 +1,20 @@
 using InvoicingGrpc.Services;
+using NodaTime;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddSingleton<IClock>(SystemClock.Instance);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ClientService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
